f11267464274e931107aea7e9375e7aef9d83944
mohsen seyedkazemi ardebili <mohsen.seyedkazemi@unibo.it>
2024-04-27 17:11:48 +0200

    data analysis

--- DataAnalysis/helper.py
+++ DataAnalysis/helper.py
@@ -7,24 +7,20 @@ from pandas import json_normalize
 def read_json(file_path):
     with open(file_path, 'rt', encoding='utf-8') as file:
         data = json.load(file)
-
-    print()
-    # display(data)
-    print("--- keys ---------------------------------------")
-    display(data[0].keys())
-    print("--- Metric Keys----------------------------------")
-    display(data[0]['metric'].keys())
+    print(f"File: {file}\n")
+    print("--- keys --------------------" + 20*'--')
+    print(data[0].keys())
+    print("--- Metric Keys -------------" + 20*'--')
+    print(data[0]['metric'].keys())
     df = json_normalize(data)
-    print("\n\n")
-    print("-+-" * 10)
-    print("\nUnique values in each column except values columns:\n")
+
+    print("\nUnique values in each column except values columns: ------------------")
     for col in df.drop(columns=['values']).columns:
-        print()
         print(f"{col}: {df[col].unique()}")
-        print()
-    print("=*=" * 10)
+    print("=-=" * 24)
     display(df)
-    print("\n\n")
+    print("\n")
+    return df
 
 
 
@@ -78,4 +74,71 @@ def json_2_pandas(json_file_path):
     csv_file_path = os.path.join(root_path+"/csv/",file_name.replace('.json', '.csv'))
     if not os.path.exists(os.path.dirname(root_path+"/csv/")):
         os.makedirs(os.path.dirname(root_path+"/csv/"))
-    df.to_csv(csv_file_path)
+    df.to_csv(csv_file_path, index=False)
+    return df
+
+
+import ast  # For converting string representation of lists into actual lists
+
+def transform_data(data):
+    # Convert the string representation of list in the 'values' column to actual lists
+    # data['values'] = data['values'].apply(ast.literal_eval)
+    
+    display(data)
+
+    # Explode the 'values' column into multiple rows
+    exploded_data = data.explode('values')
+
+    # Split the 'values' column into 'timestamp' and 'value' columns
+    exploded_data[['timestamp', 'value']] = pd.DataFrame(exploded_data['values'].tolist(), index=exploded_data.index)
+
+    # Now let's display the first few rows of the transformed DataFrame
+    display(exploded_data)
+
+    exploded_data['timestamp'] = pd.to_datetime(exploded_data['timestamp'], unit='s')
+    exploded_data.drop(columns=['values'], inplace=True, axis=0)
+    exploded_data.fillna("_", inplace=True)
+    # display(exploded_data)
+    pivoted_data = exploded_data.pivot(index='timestamp', columns=[col for col in exploded_data.columns if col not in ['value', 'timestamp']], values='value')
+
+    if isinstance(pivoted_data, pd.Series):
+        display(pivoted_data)
+        pivoted_data = pd.DataFrame(pivoted_data, columns=['val'])
+        pivoted_data.reset_index(drop=False, inplace=True)
+        pivoted_data.set_index('timestamp', inplace=True) 
+    
+    pivoted_data.sort_index(inplace=True) 
+    display(pivoted_data)
+    
+
+    # Remove the name of the column index
+    pivoted_data.columns.name = None
+    
+    pivoted_data.index.name = None
+
+    # pivoted_data.reset_index(drop=False, inplace=True)  
+    display("Pivoted Data:")
+    display(pivoted_data)
+    # Resetting the index
+
+    # pivoted_data.columns = ['_'.join(col).strip() for col in pivoted_data.columns.values]
+    # pivoted_data.columns = pivoted_data.columns.str.replace(":", "_").str.replace(" ", "-")
+
+    # Identify non-numeric columns
+    non_numeric_columns = pivoted_data.select_dtypes(exclude=['int', 'float']).columns
+    print(non_numeric_columns)
+
+    # Convert non-numeric columns to float
+    for col in non_numeric_columns:
+        pivoted_data[col] = pd.to_numeric(pivoted_data[col], errors='coerce')
+
+    display(pivoted_data)
+
+    return pivoted_data
+
+
+def unique_values(data):
+    unique_values = {col: data[col].unique() for col in data.columns}
+    for column, values in unique_values.items():
+        print(f"Unique values for {column}: {values}")
+    return unique_values
